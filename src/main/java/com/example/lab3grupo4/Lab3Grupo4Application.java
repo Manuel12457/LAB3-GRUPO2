b52058b478ec2f3bd2c80be53882da68ec23e830
Manuel <a20190133@pucp.edu.pe>
2022-04-19 19:10:56 -0500

    Proyecto Base

---
+++ src/main/java/com/example/lab3grupo4/Lab3Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Grupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Grupo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3grupo4/Lab3Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Grupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

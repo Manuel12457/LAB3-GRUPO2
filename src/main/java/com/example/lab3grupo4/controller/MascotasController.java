e6072062bcea44d487d6199b366eb29bd257e0b3
Javier Marroquin <a20193644@pucp.edu.pe>
2022-04-19 20:07:57 -0500

    Mascotas lista vista

--- src/main/java/com/example/lab3grupo4/controller/MascotasController.java
+++ src/main/java/com/example/lab3grupo4/controller/MascotasController.java
@@ -1,8 +1,10 @@
 package com.example.lab3grupo4.controller;
 
 import com.example.lab3grupo4.entity.Mascota;
+import com.example.lab3grupo4.repository.MascotasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping(value = "/mascotas")
 public class MascotasController {
+    @Autowired
+    MascotasRepository mascotasRepository;
 
-    @GetMapping(value = "/")
-    public String listaMascostas(){
+    @GetMapping(value = "")
+    public String listaMascostas(Model model){
+        model.addAttribute("listaMascotas",mascotasRepository.findAll());
         return "mascotas/lista";
     }
+
     @GetMapping(value = "/edit")
     public String editarMascota(@ModelAttribute("mascota") Mascota mascota,@RequestParam("id") Integer id){
         return "mascotas/editar";
@@ -30,7 +36,6 @@ public class MascotasController {
     }
     @GetMapping(value = "/delete")
     public String borrarMascota(@RequestParam("id") Integer id){
-
         return "mascotas/nuevo";
     }
 }
